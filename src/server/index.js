4a1051885acf3ad5709dac8a167ddf741f4ab118
anders11 <anderslatif@gmail.com>
2017-03-13 10:06:34 +0100

    Implementing SSL and HTTPS

--- src/server/index.js
+++ src/server/index.js
@@ -4,13 +4,34 @@ var Promise = require("bluebird");
 
 var app = express();
 
+// For SSL
+var https = require("https");
+var fs = require("fs"); // to read the private and public key files
+
+var privateKey = fs.readFileSync(__dirname + "/sslcert/privateSSL", 'utf8');
+var certificate = fs.readFileSync(__dirname + "/sslcert/public", 'utf8');
+var sslOptions = {"key": privateKey, "cert": certificate};
+
+var serverSSL = https.createServer(sslOptions, app);
+
+
 // syncing the models to the database and associating the mapping (persistence layer)
 var db = require('./models/db');
 
 
-
+// traditional http
 db.sequelize.sync().then( function() {
      var server = app.listen(3000, function(err) {
+        if (err) {
+            console.log('Cannot listen on port %d', server.address().port)
+        }
+        console.log('Listening on port %d', server.address().port);
+    });
+});
+
+// https and SSL
+db.sequelize.sync().then( function() {
+     var server = serverSSL.listen(3001, function(err) {
         if (err) {
             console.log('Cannot listen on port %d', server.address().port)
         }
